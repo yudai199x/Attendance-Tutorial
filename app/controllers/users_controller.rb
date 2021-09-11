2a5c951a75a6ea299738f27c9ae5df525dc5c503
藤井雄大 <zdcmg47636@yahoo.co.jp>
2021-09-11 19:36:28 +0000

    8章8.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -19,11 +22,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -37,5 +38,21 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
 :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
