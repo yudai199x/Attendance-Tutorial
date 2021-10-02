4be028794c34ac2b40379f15b2d3f7e84400da63
藤井雄大 <zdcmg47636@yahoo.co.jp>
2021-10-02 05:16:32 +0000

    Finish add-user-attendances

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -8,5 +8,9 @@ module AttendancesHelper
     end
     # どれにも当てはまらなかった場合はfalseを返します。
     false
-  end  
+  end
+  
+  def working_times(start,finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
 end
